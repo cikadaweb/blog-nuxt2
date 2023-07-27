d695594fa996be08cf1af320bc9cae7600dcbe77
CIKADA <adel.garipovazn@mail.ru>
2023-07-27 17:58:45 +0300

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button>Button</button>
+    <button class="btn_scss">Button + SCSS</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style lang="scss" scoped>
+.btn_scss {
+  @include button-reset;
+}
+</style>
