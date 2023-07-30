93dfd6b3332ee637d9b33912280af7c058f8551e
CIKADA <adel.garipovazn@mail.ru>
2023-07-31 01:19:31 +0300

    Create CommentList

---
+++ components/comments/CommentList.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="comment-list">
+    <div class="comment-list__title">Комментарии</div>
+    <li v-for="item in items" :key="item.id" class="comment-item">
+      <CommentItem :item="item"/>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import CommentItem from '@/components/comments/CommentItem.vue'
+
+import { IComment } from '@/store/posts';
+
+export default defineComponent({
+  name: 'CommentList',
+  components: {
+    CommentItem
+  },
+  props: {
+    items: {
+      type: Array as PropType<IComment[]>,
+      default() {
+        return []
+      },
+      required: true
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.comment-list {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.comment-list__title {
+  font-size: 20px;
+  margin-bottom: 40px;
+}
+</style>
