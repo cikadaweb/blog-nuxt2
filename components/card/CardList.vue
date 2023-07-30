efafe844cf11c7985394b31602fa422fbae90ca1
CIKADA <adel.garipovazn@mail.ru>
2023-07-30 16:17:02 +0300

    Create CardList

---
+++ components/card/CardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="card-list">
+    <li class="card-item" v-for="item in items" :key="item.id">
+      <CardItem :item="item"/>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { IPost } from '@/store/posts';
+
+export default defineComponent({
+  name: 'CardList',
+  props: {
+    items: {
+      type: Array as PropType<IPost[]>,
+      default() {
+        return []
+      },
+      required: true
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card-list {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
