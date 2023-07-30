2e7ead01b0a8eeee87a3556da995fce734e5e852
CIKADA <adel.garipovazn@mail.ru>
2023-07-30 21:24:05 +0300

    Create posts page

---
+++ pages/posts/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PostPage />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import PostPage from '@/components/views/PostPage.vue';
+
+export default defineComponent({
+  components: {
+    PostPage
+  }
+})
+</script>
