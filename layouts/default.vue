062ba38d1016f9ffc89dff14433d04734c8d8708
CIKADA <adel.garipovazn@mail.ru>
2023-07-31 01:18:46 +0300

    Add Alert component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
+    <AppAlert />
     <TheHeader />
     <Nuxt class="main"/>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import AppAlert from '@/components/alert/AppAlert.vue';
 import TheHeader from '@/components/header/TheHeader.vue';
 
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'default',
+  components: {
+    TheHeader, AppAlert
+  }
+});
 </script>
 
 <style lang="scss" scoped>
