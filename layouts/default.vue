495130b6269bf946273937f591c03d3dbcbd70e6
CIKADA <adel.garipovazn@mail.ru>
2023-07-30 13:32:17 +0300

    Creat default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheHeader />
+    <Nuxt class="main"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheHeader from '@/components/header/TheHeader.vue';
+
+</script>
+
+<style lang="scss" scoped>
+.main {
+  padding-top: 80px;
+}
+</style>
