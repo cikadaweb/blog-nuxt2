f68031907a76e8ddca7490fb4892da516d27594a
CIKADA <adel.garipovazn@mail.ru>
2023-07-30 21:22:44 +0300

    Create BaseButton

---
+++ components/UI/BaseButton.vue
@@ -0,0 +1,80 @@
+<template>
+  <button
+    class="button"
+    :class="[buttonColorClass]"
+    :type="type"
+    :disabled="disabled"
+    @click="clickOnButton"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseButton',
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    disabled: {
+      type: Boolean,
+      required: false
+    },
+    type: {
+      type: String as () => 'button' | 'submit' | 'reset',
+      default: 'button',
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  emits: ['click-on-button'],
+  computed: {
+    buttonColorClass() {
+      return 'btn_primary'
+    }
+  },
+  methods: {
+    clickOnButton(event: Event) {
+      this.$emit('click-on-button');
+    }
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.button {
+  @include button-reset;
+}
+.button{
+  @include font(14px, 400, 19px);
+  border-radius: $btn-radius;
+  padding: $btn-padding;
+}
+
+.btn_primary {
+  background-color: $bg-primary;
+  color: $font-white;
+  // &:hover {
+  //   background-color: $bg-primary-hover;
+  //   color: $font-secondary;
+  // }
+  // &:active {
+  //   background-color: $bg-primary-active;
+  //   color: $font-secondary;
+  // }
+  // &:disabled {
+  //   background-color: $bg-disabled;
+  //   color: $font-disabled;
+  // }
+  // &_active {
+  //   background-color: $bg-primary-active;
+  //   color: $font-secondary;
+  // }
+}
+</style>
